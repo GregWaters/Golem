2a9ee9bba41c228906b3632849ed2378674ef7f6
GregWaters <gregoryarthurwaters@gmail.com>
2024-04-09 10:54:12 -0500

    Update character module

--- character.hpp
+++ character.hpp
@@ -1,6 +1,11 @@
 #ifndef __CHARACTER_HPP
 #define __CHARACTER_HPP
 
+#include <cstddef>
+
+#define EOF (-1)
+#define WEOF (static_cast<wint_t>(-1))
+
 // Use these functions whenever possible to show intent and greatly improve readability without sacrificing performance
 
 // Standard functions for character and string processing
@@ -66,5 +71,43 @@ constexpr bool ispunct(char c)
 {
     return (c >= '!' && c <= '/') || (c >= ':' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~');
 }
+
+// Wide-character functions [TODO]
+
+// If we're able to simply overload the previous functions, use the following snippet.
+// If not, keep the active declarations below the snippet.
+
+/*
+constexpr bool isalnum(wchar_t c);
+constexpr bool isalpha(wchar_t c);
+constexpr bool islower(wchar_t c);
+constexpr bool isupper(wchar_t c);
+constexpr bool isdigit(wchar_t c);
+constexpr bool isxdigit(wchar_t c);
+constexpr bool iscntrl(wchar_t c);
+constexpr bool isgraph(wchar_t c);
+constexpr bool isspace(wchar_t c);
+constexpr bool isblank(wchar_t c);
+constexpr bool isprint(wchar_t c);
+constexpr bool ispunct(wchar_t c);
+constexpr bool tolower(wchar_t c);
+constexpr bool toupper(wchar_t c);
+*/
+
+constexpr bool iswalnum(wchar_t c);
+constexpr bool iswalpha(wchar_t c);
+constexpr bool iswlower(wchar_t c);
+constexpr bool iswupper(wchar_t c);
+constexpr bool iswdigit(wchar_t c);
+constexpr bool iswxdigit(wchar_t c);
+constexpr bool iswcntrl(wchar_t c);
+constexpr bool iswgraph(wchar_t c);
+constexpr bool iswspace(wchar_t c);
+constexpr bool iswblank(wchar_t c);
+constexpr bool iswprint(wchar_t c);
+constexpr bool iswpunct(wchar_t c);
+
+constexpr bool towlower(wchar_t c);
+constexpr bool towupper(wchar_t c);
 
 #endif

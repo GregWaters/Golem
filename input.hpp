51ed84fcbd67360e534ed420f5978cf5956e5945
GregWaters <gregoryarthurwaters@gmail.com>
2024-04-09 10:29:03 -0500

    Add template for input module

--- input.hpp
+++ input.hpp
@@ -1,6 +1,14 @@
 #ifndef __INPUT_HPP
 #define __INPUT_HPP
 
+template <typename T>
+void input(T& ptr) // TODO
+{
+    #if defined(OS_LINUX)
+    
+    #elif defined(OS_WINDOWS)
 
+    #endif
+}
 
 #endif // __INPUT_HPP

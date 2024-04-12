c16eabdb62670ac1ac69684e4e0a11ef028d211e
GregWaters <gregoryarthurwaters@gmail.com>
2024-04-12 16:30:42 -0500

    Stuff?? I can't remember

--- modules/bigint.hpp
+++ modules/bigint.hpp
@@ -4,15 +4,36 @@
 // Use native assembly for the bigint class
 // For x86_64, I believe this is the `ADCX` instruction
 #include "cpuarch.hpp"
-#include <vector>
 
+#include <vector>
+#include <cstdint>
+#include <cmath>
+#include <string>
+#include <limits>
+
+// Arbitrary precision signed integer (naive implementation)
+// Might not even compile, I don't know yet
 class bigint
 {
+private:
+    std::vector<unsigned int> m_internal;
     bool m_sign = false;
-    std::vector<unsigned char> m_bytes;
+
+    inline size_t& operator[](size_t index)
+    {
+        return m_internal[index];
+    }
 
 public:
-    // TODO
+    operator+=(unsigned int value)
+    {
+        size_t index = 0;
+
+        while (overflow)
+        {
+            ++index;
+        }
+    }
 };
 
 #endif // __BIGINT_HPP

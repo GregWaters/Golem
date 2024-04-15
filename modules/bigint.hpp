27cb7724f49f509e736a201138d5952cfd66b960
GregWaters <gregoryarthurwaters@gmail.com>
2024-04-15 14:09:00 -0500

    Update bigint

--- modules/bigint.hpp
+++ modules/bigint.hpp
@@ -1,38 +1,142 @@
 #ifndef __BIGINT_HPP
 #define __BIGINT_HPP
 
-// Use native assembly for the bigint class
-// For x86_64, I believe this is the `ADCX` instruction
-#include "cpuarch.hpp"
 
 #include <vector>
-#include <cstdint>
-#include <cmath>
-#include <string>
-#include <limits>
+#include <cstdlib>
 
-// Arbitrary precision signed integer (naive implementation)
-// Might not even compile, I don't know yet
-class bigint
+// Bigint class -- inherits from `long_array` with a size_type of `unsigned int`
+// If anyone encounters an overflow issue, please check your supercomputer in at the door, as the max value is so large that my calculator just prints `infinity`
+// Have fun with it!
+class unsigned_bigint
 {
-private:
-    std::vector<unsigned int> m_internal;
-    bool m_sign = false;
+    // It's not a raw pointer if you just let me cook
+    unsigned int* m_internal;
+    unsigned int m_size;
 
-    inline size_t& operator[](size_t index)
+
+    // Get current size
+    size_type size() const
+    {
+        return m_size;
+    }
+
+
+    // Allocate memory for `n` integers -- typically called within a constructor
+    // Note that there should be a call to `deallocate()` for every call to `allocate()`
+    void allocate(const size_type n)
+    {
+        m_internal = static_cast<value_type*>(std::malloc(n * sizeof(value_type)));
+    }
+
+
+    // Free all memory -- typically called within a destructor
+    void deallocate()
+    {
+        std::free(m_internal);
+    }
+
+
+    // Debug tool
+    void print()
+    {
+        std::putchar('[');
+        for (int i = 0; i < size(); ++i)
+            std::printf("%010d, ", m_internal[i]);
+        std::putchar(']');
+    }
+
+
+    // Reserve `amount` integers in the internal array
+    void reserve(size_type amount)
+    {
+        m_internal = std::realloc(m_internal, amount * sizeof(size_type));
+        
+        if (m_internal == nullptr)
+            throw std::bad_alloc;
+    }
+
+
+    // Random access operator
+    value_type& operator[](size_type index)
     {
         return m_internal[index];
     }
 
-public:
-    operator+=(unsigned int value)
-    {
-        size_t index = 0;
 
-        while (overflow)
-        {
-            ++index;
-        }
+public:
+    unsigned_bigint()
+    {
+        allocate(4);
+    }
+
+
+    ~unsigned_bigint()
+    {
+        deallocate();
+    }
+
+
+    // Conversions
+
+    operator int()
+    {
+
+    }
+
+    operator double()
+    {
+        
+    }
+    
+
+
+    operator+(unsigned int x) const;
+    operator-(unsigned int x) const;
+    operator*(unsigned int x) const;
+    operator/(unsigned int x) const;
+    operator+=(unsigned int x);
+    operator-=(unsigned int x);
+    operator*=(unsigned int x);
+    operator/=(unsigned int x);
+    
+    
+    // Boolean operations
+    
+    operator!=(unsigned_bigint b) const
+    {
+        if (b.size() != size())
+            return true;
+        
+        for (int i = 0; i < size(); ++i)
+            if (b.m_internal[i] != m_internal[i])
+                return true;
+        return false;
+    }
+
+
+    operator==(unsigned_bigint b) const
+    {
+        // lol ez
+        return !(operator!=(b));
+    }
+
+
+    operator<(unsigned_bigint b) const;
+    operator>(unsigned_bigint b) const;
+    operator<=(unsigned_bigint b) const;
+    operator>=(unsigned_bigint b) const;
+};
+
+class bigint
+{
+    bool m_sign;
+
+public:
+    // Get the sign bit
+    bool sign()
+    {
+        return m_sign;
     }
 };
 

84410b65362b4466d38b63a61322d0a42d31805c
Greg Waters <gregoryarthurwaters@gmail.com>
2024-08-12 08:06:10 -0500

    Reformat lexer header

--- inc/lexer.h
+++ inc/lexer.h
@@ -7,56 +7,56 @@
 
 enum token_desc
 {
-    // Signal tokens
-    TK_END, // End of token stream
-
+    // Signals
+    TK_END         , // End of token stream
+    
     // Punctuation
-    TK_LBRACE, // {
-    TK_RBRACE, // }
-    TK_LBRACK, // [
-    TK_RBRACK, // ]
-    TK_LPAREN, // (
-    TK_RPAREN, // )
-    TK_PLUS,     // +
-    TK_MINUS,    // -
-    TK_ARROW,    // ->
-    TK_ASTERISK, // *
-    TK_SLASH,    // /
-    TK_MODULO,   // %
-    TK_NOT,    // ~
-    TK_AND,    // &
-    TK_OR,     // |
-    TK_XOR,    // ^
-    TK_LSHIFT, // <<
-    TK_RSHIFT, // >>
-    TK_EQUALS,     // =
-    TK_GREATER,    // >
-    TK_GREATER_EQ, // >=
-    TK_LESS,       // <
-    TK_LESS_EQ,    // <=
-    TK_HASHTAG, // #
-    TK_AT,      // @
-    TK_COMMA,   // ,
-    TK_DOT,     // .
-    TK_GRAVE,   // `
-
+    TK_LBRACE      , // {
+    TK_RBRACE      , // }
+    TK_LBRACK      , // [
+    TK_RBRACK      , // ]
+    TK_LPAREN      , // (
+    TK_RPAREN      , // )
+    TK_PLUS        , // +
+    TK_MINUS       , // -
+    TK_ARROW       , // ->
+    TK_ASTERISK    , // *
+    TK_SLASH       , // /
+    TK_MODULO      , // %
+    TK_NOT         , // ~
+    TK_AND         , // &
+    TK_OR          , // |
+    TK_XOR         , // ^
+    TK_LSHIFT      , // <<
+    TK_RSHIFT      , // >>
+    TK_EQUALS      , // =
+    TK_GREATER     , // >
+    TK_GREATER_EQ  , // >=
+    TK_LESS        , // <
+    TK_LESS_EQ     , // <=
+    TK_HASHTAG     , // #
+    TK_AT          , // @
+    TK_COMMA       , // ,
+    TK_DOT         , // .
+    TK_GRAVE       , // `
+    
     // Literals
-    TK_STRING,  // "
-    TK_CHAR,    // '
-    TK_NUMBER,  // 0123456789
+    TK_STRING      , // "
+    TK_CHAR        , // '
+    TK_NUMBER      , // 0123456789
 
     // Keywords
-    KW_IF,
-    KW_INT,     // Integer variable declaration
-    KW_FLOAT,   // Floating-point variable declaration
-    KW_CLASS,   // Class declaration
-    KW_FUNC,    // Function declaration
-    KW_MACRO,   // Macro declaration
-    KW_TYPE,    // Type definition
-    KW_NULL,    // Representation of 'nothing'
+    KW_IF          , // Checks for a boolean expression to its immediate right and branches accordingly
+    KW_INT         , // Integer variable declaration
+    KW_FLOAT       , // Floating-point variable declaration
+    KW_CLASS       , // Class declaration
+    KW_FUNC        , // Function declaration
+    KW_MACRO       , // Macro declaration
+    KW_TYPE        , // Type definition
+    KW_NULL        , // Representation of 'nothing'
 
     // Identifiers
-    TK_IDENT,    // User-defined
+    TK_IDENT       , // User-defined
 };
 
 struct Token
